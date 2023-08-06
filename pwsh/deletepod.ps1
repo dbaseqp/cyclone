cfee8c4698e4deb3a33c951a11ac0d4bc5e01eb1
evanjd711 <evanjd711@gmail.com>
2023-08-06 13:02:32 -0700

    fixed creds

--- pwsh/deletepod.ps1
+++ pwsh/deletepod.ps1
@@ -3,7 +3,7 @@ param(
 [String] $Tag
 )
 
-#$cred = Import-CliXML -Path .\pwsh\vsphere_cred.xml
-Connect-VIServer elsa.sdc.cpp -User bruharmycloner -Password '$w1ftCCDC!123'#-Credential $cred
+$cred = Import-CliXML -Path .\pwsh\vsphere_cred.xml
+Connect-VIServer elsa.sdc.cpp -Credential $cred
 Invoke-OrderSixtySix -Username $Username -Tag $Tag
 
