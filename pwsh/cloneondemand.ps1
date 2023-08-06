cdccf89fcd27cafb770d980731a4db66a2d9512d
dbaseqp <gabrielfok128@gmail.com>
2023-08-06 10:16:17 -0700

    fixed paths

--- pwsh/cloneondemand.ps1
+++ pwsh/cloneondemand.ps1
@@ -10,8 +10,7 @@ param(
 
 $pg = [int] $Port
 
-$cred = Import-CliXML -Path .\vsphere_cred.xml
+$cred = Import-CliXML -Path .\pwsh\vsphere_cred.xml
 $session = Connect-VIServer elsa.sdc.cpp -Credential $cred
-$session.SessionID > .\session.txt
 
 Invoke-WebClone -SourceResourcePool $Template -Target $Target -Portgroup $pg -Domain $domain -WanPortGroup $WanPG -Username $Username -Password $Password
